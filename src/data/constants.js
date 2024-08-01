b379c546b2de6c53a15947cc9f030888550e7eae
Sourav Saha <102205180+souravsaha51@users.noreply.github.com>
2024-08-02 02:23:54 +0530

    Add files via upload

---
+++ src/data/constants.js
@@ -0,0 +1,316 @@
+export const Bio = {
+    name: "Sourav Saha",
+    roles: [
+      "Programmer",      
+      "Frontend Developer",
+      "Full Stack Developer"
+    ],
+    description:
+      "I'm a pre-final year student at NIT Agartala. I am a motivated and versatile individual, always eager to take on new challenges. I am dedicated to delivering high-quality results With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/souravsaha51",
+    resume:
+      "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/sourav-saha-4b89b7227/",
+    twitter: "https://x.com/LetsSourav1",
+    GitHub: "https://github.com/souravsaha51",
+  };
+  
+  export const skills = [
+    { 
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "https://nodejs.org/static/images/logo.svg",
+        },
+        {
+          name: "Express Js",
+          image:
+            "https://cdn.prod.website-files.com/609bc2fa29b6d5b7f44a2785/647743f51bc76753239a8bc6_expressjs-logo.webp",
+        },
+
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        {
+          name: "Postgresql",
+          image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+        },
+        {
+          name: "MongoDB",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        },
+
+      ],
+    },
+ 
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Git",
+          image:
+            "https://cdn.iconscout.com/icon/free/png-512/free-social-226-96741.png?f=webp&w=256",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "Docker",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+        },
+ 
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Postman",
+          image:
+            "https://cdn.iconscout.com/icon/free/png-512/free-postman-3521648-2945092.png?f=webp&w=256",
+        },
+
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+      role: "DevOps & Fullstack Engineering Intern",
+      company: "Flipr Inovations Pvt. Ltd.",
+      date: "Aug 2023 - July 2023",
+      desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
+      skills: [
+        "Docker",
+        "Terraform",
+        "AWS",
+        "EC2",
+        "Portainer",
+        "Nginx",
+        "JavaScript",
+        "TypeScript",
+        "Node Js",
+        " Next Js",
+      ],
+      doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+    },
+    {
+      id: 0,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/neurobit.jpeg?alt=media&token=1cacbb7b-e5ea-4efb-9b3d-f5a31b9564ad",
+      role: "Frontend Engineer Intern",
+      company: "Neurobit",
+      date: "June 2023 - Present",
+      desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
+      skills: [
+        "ReactJS",
+        "Redux",
+        "NodeJs",
+        "Material UI",
+        "HTML",
+        "CSS",
+        "JavaScript",
+      ],
+      doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+    },
+    {
+      id: 1,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+      role: "Fullstack Externship",
+      company: "Flipr",
+      date: "June 2023 - July 2023",
+      desc: "Built an employee management full stack web app used Docker and deployed on AWS ec2. I was the top performer in the program.",
+      skills: [
+        "ReactJS",
+        "Redux",
+        "NodeJs",
+        "Material UI",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "Docker",
+        "AWS",
+        "MongoDB",
+      ],
+    },
+    {
+      id: 2,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+      role: "Open Source Contributor ",
+      company: "GirlScript Summer of Code",
+      date: "May 2023 - Present",
+      desc: "Contributing to different open-source projects and learn from industry experts",
+      doc: "https://media.licdn.com/dms/image/D4D22AQGzSpgFaR4hsQ/feedshare-shrink_800/0/1684079632872?e=1687996800&v=beta&t=i0xWn_k32Oj9SxKfc6rARSbOghXD9Ge6WyEb2p2vvrc",
+    },
+    {
+      id: 3,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/gdsc.jpeg?alt=media&token=c162329c-efaa-4be8-a173-8d3f4c48ea70",
+      role: "Android Developer",
+      company: "DSC KIIT",
+      date: "Nov2021 - Present",
+      desc: "As an Android developer at the Google Developers Student Club (GDCS), I have had the opportunity to work on exciting projects and collaborate with talented developers who share my passion for technology. Through my involvement with GDCS, I have also had the opportunity to host and participate in numerous events, including hackathons, study jams, and workshops.",
+      skills: [
+        "Leadership",
+        "Mobile Application Development",
+        "Kotlin",
+        "XML",
+        "Figma",
+      ],
+    },
+    {
+      id: 4,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Rudraksha.jpeg?alt=media&token=8f83f41e-d0a1-486f-9c7f-dd4cd1d09e8d",
+      role: "Android Developer Intern",
+      company: "Rudraksha Welffare Foundation",
+      date: "June 2021 - Oct 2021",
+      desc: "As an Android Developer Intern at Rudraksha Welfare Foundation from June 2021 to October 2021, I gained valuable hands-on experience in application development. During my tenure, I contributed to the development of the application's back-end cloud functions using Firebase and implemented a front-end with the MVVM structure, which was designed using Figma. I also added a one-to-one video call feature, conceptualized new features, and optimized app performance. This internship helped me enhance my skills in Android development and gave me an opportunity to work in a collaborative team environment.",
+      skills: [
+        "Android",
+        "Java",
+        "Kotlin",
+        "XML",
+        "Node Js",
+        "Cloud Firestore",
+        "Firebase",
+        "Figma",
+      ],
+      doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/WhatsApp%20Image%202023-05-05%20at%2012.07.39%20AM.jpeg?alt=media&token=9f0e1648-568b-422d-bd0b-1f125f722245",
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://en.wikipedia.org/wiki/File:National_Institute_of_Technology,_Agartala_Logo.png ",
+      school: "Kalinga Institute of Industrial Technology, Bhubaneswar",
+      date: "Oct 2021 - Sep 2025",
+      grade: "8.71 CGPA",
+      desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Kalinga Institute of Industrial Technology, Bhubaneswar. I have completed 4 semesters and have a CGPA of 8.71. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Google Developers Student Club (GDSC) at KIIT, where I am learning and working on exciting projects with a team of talented developers.",
+      degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+    },
+    {
+      id: 1,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
+      school: "Methodist School, Dankuni",
+      date: "Apr 2019 - Apr 2025",
+      grade: "88.2%",
+      desc: "I completed my class 12 high school education at Methodist School, Dankuni, where I studied Science with Computer Science.",
+      degree: "ISC(XII), Science with Computer",
+    },
+    {
+      id: 2,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
+      school: "Methodist School, Dankuni",
+      date: "Apr 2017 - Apr 2019",
+      grade: "92.3%",
+      desc: "I completed my class 10 education at Methodist School, Dankuni, where I studied Science with Computer Application.",
+      degree: "ICSC(X), Science with Computer",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 9,
+      title: "Weather App",
+      date: "Feb 2023 - Mar 2023",
+      description:
+        "Developed a global weather forecasting website using the OpenWeather API to provide real-time conditions and forecasts for any city worldwide.",
+      image:
+        "https://github.com/souravsaha51/Weather-App/blob/main/weather_pic.png?raw=true",
+      tags: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+
+      ],
+      category: "web app",
+      github: "https://github.com/souravsaha51/Weather-App",
+      webapp: "https://monumental-strudel-0f38e6.netlify.app",
+    },
+    {
+      id: 0,
+      title: "Bangalore-House-Price-Prediction",
+      date: "Jun 2024 - Jul 2024",
+      description:
+        "This project combines machine learning and web development to predict housing prices for individuals relocating within Bangalore, Karnataka, India.",
+      image:
+        "https://github.com/souravsaha51/Bangalore-House-Price-Prediction/blob/main/bhp_pic.png?raw=true",
+      tags: ["Python", "Flask", "Linear Regression", "Machine Learning", "Prediction"],
+      category: "machine learning",
+      github: "https://www.kaggle.com/datasets/sarthakniwate13/bangalore-house-price-dataset",
+      webapp: "https://github.com/souravsaha51/Bangalore-House-Price-Prediction",
+     
+    },
+    {
+      id: 1,
+      title: "Super Store Sales Dashboard",
+      date: "May 2024 - Jun 2024",
+      description:
+        "The Superstore Power BI Dashboard project provides in-depth analysis of Superstore data, delivering actionable insights to drive targeted growth, improve profitability, and enhance decision-making.",
+      image:
+        "https://github.com/souravsaha51/Super-Store-Sales-Analysis/blob/main/Dashboard.png?raw=true",
+      tags: [
+        "Data Visualization",
+        "Data Cleaning",
+        "Data Transformation",
+        "Data Extraction",
+        "Power BI",
+      ],
+      category: "android app",
+      github: "https://github.com/souravsaha51/Super-Store-Sales-Analysis",
+      webapp: "https://github.com/souravsaha51/Super-Store-Sales-Analysis/blob/main/Dashboard.png?raw=true",
+    }, 
+
+  ];
+  
+  export const TimeLineData = [
+    { year: 2017, text: "Started my journey" },
+    { year: 2018, text: "Worked as a freelance developer" },
+    { year: 2019, text: "Founded JavaScript Mastery" },
+    { year: 2020, text: "Shared my projects with the world" },
+    { year: 2021, text: "Started my own platform" },
+  ];
